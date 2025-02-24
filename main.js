c211ebd64ffc27284f7ea9e588815160662fb2ed
leonardopalluzzi <lp.palluzzi@gmail.com>
2025-02-24 16:43:56 +0100

    add on page print function

--- main.js
+++ main.js
@@ -13,40 +13,44 @@ const teamMembers = [
         email: 'giannonematto@gmail.com'
     },
 ];
-const cardEl = document.querySelector('.card_container');
+const cardEl = document.querySelector('.row');
 
 
 // generate html code for cards
 function getCardMarkup (arr){
+    const printArray = [];
     let markup;
     for(let i = 0; i < arr.length; i++){
         const {name, job, email} = arr[i];
-        markup += 
-        `<div class="card_custom d-flex flex-row gap-2">
-            <img class="card_img" src="https://picsum.photos/id/237/300" alt="">
-            <div class="text_card">
-                <h3 id="name">${name}</h3>
-                <span id="job">${job}</span>
-                <a id="email" href="">${email}</a>
+        printArray[i] =
+        `<div class="col-4">
+            <div class="card_custom d-flex flex-row gap-2">
+                <img class="card_img" src="https://picsum.photos/id/237/300" alt="">
+                <div class="text_card">
+                    <h3 id="name">${name}</h3>
+                    <span id="job">${job}</span>
+                    <a id="email" href="">${email}</a>
+                </div>
             </div>
-        </div>`;
+        </div>`
     }
-    return markup;
+    
+    return printArray;
 }
 
-console.log(getCardMarkup(teamMembers));
+console.log(getCardMarkup(teamMembers, cardEl));
 
-getCardMarkup(teamMembers);
+getCardMarkup(teamMembers,cardEl);
 
 //print array function
-function getMembers() {
-
-
-    
-
-
+function getMembers(markupArr, container) {
+    for(let i = 0; i < markupArr.length; i++){
+        container.innerHTML += markupArr[i];
+    }
+    return container.innerHTML
 }
 
+getMembers(getCardMarkup(teamMembers), cardEl);
 
 //add object to array function
 
