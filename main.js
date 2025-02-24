d9316c45ba15ea5cdde4438e7622ae9a697d37a9
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-24 18:05:12 +0100

    add form inputs read

--- main.js
+++ main.js
@@ -26,16 +26,17 @@ const teamMembers = [
     },
 ];
 const cardEl = document.querySelector('.row');
+const formEl = document.querySelector('.add_form');
 
 
 // generate html code for cards
-function getCardMarkup (arr){
+function getCardMarkup(arr) {
     const printArray = [];
     let markup;
-    for(let i = 0; i < arr.length; i++){
-        const {name, job, email} = arr[i];
+    for (let i = 0; i < arr.length; i++) {
+        const { name, job, email } = arr[i];
         printArray[i] =
-        `<div class="col-4 mb-4">
+            `<div class="col-4 mb-4">
             <div class="card_custom d-flex flex-column flex-lg-row gap-2">
                 <img class="card_img" src="https://picsum.photos/id/237/300" alt="">
                 <div class="text_card">
@@ -46,17 +47,17 @@ function getCardMarkup (arr){
             </div>
         </div>`
     }
-    
+
     return printArray;
 }
 
 console.log(getCardMarkup(teamMembers, cardEl));
 
-getCardMarkup(teamMembers,cardEl);
+getCardMarkup(teamMembers, cardEl);
 
 //print array function
 function getMembers(markupArr, container) {
-    for(let i = 0; i < markupArr.length; i++){
+    for (let i = 0; i < markupArr.length; i++) {
         container.innerHTML += markupArr[i];
     }
     return container.innerHTML
@@ -65,6 +66,16 @@ function getMembers(markupArr, container) {
 getMembers(getCardMarkup(teamMembers), cardEl);
 
 //add object to array function
+formEl.addEventListener('submit', e => {
+    e.preventDefault();
+    const nameEl = document.getElementById('name').value;
+    const jobEl = document.getElementById('job').value;
+    const emailEl = document.getElementById('email').value;
+
+    console.log(nameEl, jobEl, emailEl);
+    
+
+})
 
 
 //add event listener to add names to the array
